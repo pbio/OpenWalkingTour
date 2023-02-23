9b36cda01e25665c60b92f59391bb3503feb2f4b
Paul Bioche <pebioche@gmail.com>
2023-02-23 18:56:48 +0100

    commit removed files

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import schema from './schema/schema.js';
+import schema from '../front/src/pages/api/schema/schema.js.js';
 
 
 const app = express();
@@ -17,7 +17,7 @@ app.use('/graphql', graphqlHTTP({
 
 mongoose
     .connect("mongodb+srv://pebioche:AQ44vFNyo7jp94hq@cluster0.l9ehctp.mongodb.net/?retryWrites=true&w=majority")
-mongoose.connection.once('open', ()=> {
+mongoose.connection.once('open', () => {
         console.log('connected to the DB'); 
         try {
             app.listen(4000, ()=> { console.log('now listening for requests on port 4000')});
